64caecb34f5c2a562ad5d76aac8a121afb7feac9
FloppidyDingo <2014jamesjackson@gmail.com>
2021-08-27 03:21:55 -0700

    string splitting function added

--- headers/Utils.h
+++ headers/Utils.h
@@ -33,3 +33,5 @@ bool load_wav(const std::string& filename, std::uint8_t& channels, std::int32_t&
 std::int32_t convert_to_int(char* buffer, std::size_t len);
 
 bool load_wav_file_header(std::ifstream& file, std::uint8_t& channels, std::int32_t& sampleRate, std::uint8_t& bitsPerSample, ALsizei& size);
+
+void stringSplit(std::string const& str, const char delim, std::vector<std::string>& out);
